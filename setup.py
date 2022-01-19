6a067fb1389a4a6ecfbd6190d02d1e740925e738
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2022-01-19 20:09:44 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'numpy>=1.18.1',
         'matplotlib>=3.2.1',
 	'fenics==2019.1.0',
-	'mshr==2019.1.0-py38hf9f41d3_3',
+	'mshr<=2019.1.0',
 	'scipy>=1.4.1',
 	'pytorch>=1.5.0'
     ]
