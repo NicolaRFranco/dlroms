9c8d5a1a1803c546314a454afb39eee3c55a4ba5
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2022-01-19 19:36:28 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+setup(
+    name='dlrom',
+    version='0.1.0',
+    packages=find_packages(include=['dlrom', 'dlrom.*']),
+    install_requires=[
+        'ipython>=7.15.0',
+        'numpy>=1.18.1',
+        'matplotlib>=3.2.1',
+	'fenics==2019.1.0',
+	'mshr==2019.1.0',
+	'scipy>=1.4.1',
+	'pytorch>=1.5.0'
+    ]
+)
