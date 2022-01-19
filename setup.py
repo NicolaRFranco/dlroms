9910936e9adf5d05d0317c0697290164d37bf2d0
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2022-01-19 22:19:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'numpy>=1.18.1',
         'matplotlib>=3.2.1',
 	'fenics==2019.1.0',
-	'mshr@https://bitbucket.org/fenics-project/mshr/src/master#egg=mshr',
+	'mshr@https://anaconda.org/conda-forge/mshr/2019.1.0/download/linux-64/mshr-2019.1.0-py38hbaebfaf_5.tar.bz2',
 	'scipy>=1.4.1',
 	'pytorch>=1.5.0'
     ]
