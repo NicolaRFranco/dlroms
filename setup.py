856eea0678a95962b26948c4be452ee5c76e08f2
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2022-01-19 22:42:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'numpy>=1.18.1',
         'matplotlib>=3.2.1',
 	'fenics==2019.1.0',
-	'mshr@https://github.com/NicolaRFranco/dlrom/blob/main/mshr.zip',
+	'mshr@git+ssh:https://git@github.com/NicolaRFranco/dlrom/mshr.zip',
 	'scipy>=1.4.1',
 	'pytorch>=1.5.0'
     ]
