3e3263a49391d59ffb1074cb7eb30b5cdaeeb8b3
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2022-04-26 15:30:28 +0100

    Update gp.py

--- src/dlroms/gp.py
+++ src/dlroms/gp.py
@@ -4,8 +4,28 @@ import numpy as np
 import scipy.sparse.linalg as spla
 
 class GaussianRandomField(object):
+    """Class for managing isotropic Gaussian random fields over general domains. Objects of this class have the following attributes,
+    
+    Attributes
+        cov             (function)          A function describing the covariance kernel. Such function should only accept a single argument, the
+                                            latter being the distance between two points. Namely, if G is the random field, then cov(|x_i - x_j|)
+                                            should return the covariance between G(x_i) and G(x_j). Clearly, this only allows for isotropic fields.
+        n               (int)               Rank at which the field is approximated.
+        singvalues      (numpy.ndarray)     Square roots of the covariance kernel eigenvalues.
+        eigenfunctions  (numpy.ndarray)     Eigenfunctions of the covariance kernel. This are stored in a k x n matrix, where k is the spatial
+                                            dimension (i.e. number of mesh vertices in the discretized domain), while n is the number of computed
+                                            eigenfunctions (which equals self.n).                                
+    """
     
     def __init__(self, mesh, kernel, upto):
+    """Constructs a Gaussian random field object.
+    
+    Input
+        mesh    (dolfin.cpp.mesh.Mesh)      Mesh discretizing the spatial domain.
+        kernel  (function)                  Covariance kernel (cf. dlroms.gp.GaussianRandomField).
+        upto    (int)                       Number of eigenfunctions to compute. Equivalently, rank at which the process is 
+                                            approximated via its Karhunen-Loeve expansion.
+    """
         self.cov = kernel
         self.n = upto
         self.singvalues, self.eigenfunctions = KarhunenLoeve(mesh, self.cov, self.n)
@@ -21,6 +41,20 @@ class GaussianRandomField(object):
             return v
         
 def KarhunenLoeve(mesh, covariance, nphis):
+    """Solves the eigenvalue problem for a given covariance operator.
+    
+    Input
+        mesh        (dolfin.cpp.mesh.Mesh)      Mesh discretizing the spatial domain.
+        covariance  (function)                  Covariance kernel (isotropic case). See dlroms.gp.GaussianRandomFields.
+        nphis       (int)                       Number of eigenfunctions to compute.
+        
+    Output
+        (tuple of numpy.ndarray). Returns eigenvalues and the eigenfunctions. The former are stored in a vector of length nphis,
+        while the latter are written in a matrix k x nphis, where k = mesh.num_vertices().
+        
+    Remark. The eigenvalue problem is: find lambda and u such that the integral of covariance(|x-y|)u(y)dy equals lambda*u(x).
+    The latter is solved via Galerkin projection over the space of P1-Finite Elements.
+    """
     
     def solve_covariance_EVP(cov, k):
         V = dolfin.function.functionspace.FunctionSpace(mesh, 'P', 1)
