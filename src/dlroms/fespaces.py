4b84784528310c80d157bd186feac718c1d7ec4e
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2022-07-11 22:31:57 +0100

    Update fespaces.py

--- src/dlroms/fespaces.py
+++ src/dlroms/fespaces.py
@@ -4,13 +4,16 @@ import torch
 import dolfin
 import mshr
 from dlroms import gifs
-daje
+from ufl.finiteelement.mixedelement import VectorElement, FiniteElement
+from ufl.finiteelement.enrichedelement import NodalEnrichedElement
+from fenics import FunctionSpace
+
 dx = dolfin.dx
 ds = dolfin.ds
 grad = dolfin.grad
 inner = dolfin.inner
 
-def space(mesh, obj, deg):
+def space(mesh, obj, deg, scalar = True, bubble = False):
     """Returns the Finite Element (FE) space of specified type (e.g. continuous/discontinuous galerkin) and degree.
     Note: only constructs FE spaces of scalar-valued functions.
     
@@ -18,32 +21,26 @@ def space(mesh, obj, deg):
         mesh    (dolfin.cpp.mesh.Mesh)  Underlying mesh of reference
         obj     (str)                   Type of space. 'CG' = Continuous Galerkin, 'DG' = Discontinuous Galerkin
         deg     (int)                   Polynomial degree at each element
+        scalar  (bool)                  Whether the space consists of scalar or vector-valued functions (in which
+                                        case scalar == True and scalar == False respectively). Defaults to True.
+        bubble  (bool)                  If True, enriches each element with bubble polynomials. Defaults to False.
         
     Output
         (dolfin.function.functionspace.FunctionSpace).
     """
-    return dolfin.function.functionspace.FunctionSpace(mesh, obj, deg)
-
-def dofmap(V, inverse = False):
-    """Mesh vertices can be ordered geometrically or algebraically (which makes quadrature formulas more efficient).
-    The present function provides a way to switch between the two.
-    
-    Input
-        V       (dolfin.function.functionspace.FunctionSpace)   Functional space (see e.g. dlroms.fespaces.space) of P1 type.
-                                                                It may describe scalar or vector fields, but they must be
-                                                                Continuous Galerkin elements of degree 1.                                                          
-        
-        inverse (bool)                                          If True, yields a vector I such that I[j] is the index of
-                                                                the jth quadrature node accordingly to the geometric ordering.
-                                                                If False, I[j] is the index in 'quadrature ordering' of the
-                                                                jth 'geometric' node.
-    Output
-        (numpy.ndarray).
-    """
-    if(inverse):
-        return dolfin.cpp.fem.dof_to_vertex_map(V)
+    if(scalar):
+        if(bubble):
+            element = FiniteElement("CG", mesh.ufl_cell(), 1) +  FiniteElement("Bubble", mesh.ufl_cell(), mesh.topology().dim() + 1)
+        else:
+            element = FiniteElement("CG", mesh.ufl_cell(), 1)
     else:
-        return dolfin.cpp.fem.vertex_to_dof_map(V)
+        if(bubble):
+            element = VectorElement(NodalEnrichedElement(FiniteElement("CG",       mesh.ufl_cell(), 1),
+                                                         FiniteElement("Bubble",   mesh.ufl_cell(), mesh.topology().dim() + 1)))
+        else:
+            element = VectorElement("CG", mesh.ufl_cell(), 1)
+    
+    return FunctionSpace(mesh, element)
 
 def boundary(mesh):
     """Returns the indexes of those nodes that lie on the boundary of a given domain.
@@ -190,51 +187,25 @@ def unitcubemesh(n, ny = None, nz = None):
     n2 = n if (nz == None) else nz
     return dolfin.cpp.generation.UnitCubeMesh(n,n1,n2)
 
-def asvector(u, mesh, obj = 'CG', deg = 1):
+def asvector(u, space):
     """Given a vector of dof values, returns the corresponding object in the functional space.
     
     Input
-        u       (numpy.ndarray or torch.Tensor)     Vector collecting the values of the function at the
-                                                    degrees of freedom. If u has shape (,n), then
-                                                    the functional space of interest should have n dof.
-                                                    If u has space (k,n), then the vector field
-                                                    having components u[0],..., u[k-1] is returned.
-        mesh    (dolfin.cpp.mesh.Mesh)              Mesh of reference.
-        obj     (str)                               Type of FE objects (cf. dlroms.fespaces.space).
-        deg     (int)                               Polynomial degree at the mesh elements.
+        u       (numpy.ndarray or torch.Tensor)                     Vector collecting the values of the function at the
+                                                                    degrees of freedom. If u has shape (,n), then
+                                                                    the functional space of interest should have n dof.
+                                                                    
+        space   (dolfin.function.functionspace.FunctionSpace).      Functional space where u belongs.
     
     Output    
         (dolfin.function.function.Function). 
         
-    Ex: let x, y = mesh.coordinates().T. Then the vector u = x**2 - y has length
-    n = mesh.num_vertices(), and contains the nodal values of the corresponding function f(a,b) = a**2 - b. The vector
-    of values u can be converted into an element of the functional space P1-FE (Continuous Galerking elements of degree 1)
-    via asvector(u, mesh).
     """
-    if(isinstance(u, numpy.ndarray)):
-        un = u
-    elif(isinstance(u, torch.Tensor)):
-        un = u.cpu().numpy()
-    else:
-        raise RuntimeError("Expected numpy.ndarray or torch.Tensor as first argument.")
-    if(len(u.shape)==1):
-        V = dolfin.function.functionspace.FunctionSpace(mesh, obj, deg)
-        uv = dolfin.function.function.Function(V) 
-        if(len(uv.vector()[:])!=len(u)):
-            raise RuntimeError("Wrong length: the number of entries in u is not compatible with the specified FE object.")
-        if(obj == 'CG' and deg == 1):
-            uv.vector()[:] = un[dolfin.cpp.fem.dof_to_vertex_map(V)]
-        else:
-            uv.vector()[:] = un
-        return uv
-    else:
-        ux, uy = u
-        uvx, uvy = asvector(ux, mesh, obj, deg), asvector(uy, mesh, obj, deg)
-        return dolfin.fem.projection.project(uvx*dolfin.function.constant.Constant((1.0, 0.0))+
-                                             uvy*dolfin.function.constant.Constant((0.0, 1.0)),
-                                             dolfin.function.functionspace.VectorFunctionSpace(mesh, obj, degree = deg, dim = 2))
+    uv = Function(space)
+    uv.vector()[:] = u
+    return uv
     
-def plot(*args, obj = 'CG', deg = 1, vmin = None, vmax = None, colorbar = False, axis = "off", shrink = 0.8):
+def plot(obj, space = None, vmin = None, vmax = None, colorbar = False, axis = "off", shrink = 0.8):
     """Plots mesh and functional objects.
     
     Input
@@ -254,24 +225,22 @@ def plot(*args, obj = 'CG', deg = 1, vmin = None, vmax = None, colorbar = False,
         axis        (obj)       Axis specifics (cf. matplotlib.pyplot.axis). Defaults to "off", thus hiding the axis.
         shrink      (float)     Shrinks the colorbar by the specified factor (defaults to 0.8). Ignored if colorbar = False.
     """
-    if(len(args)==2):
-        uv = asvector(*args, obj, deg)
-        if(len(args[0].shape)==1):
-            c = dolfin.common.plotting.plot(uv, vmin = vmin, vmax = vmax)
+    try:
+        if(space == None)):
+            dolfin.common.plotting.plot(obj)
         else:
-            c = dolfin.common.plotting.plot(uv)
-        if(colorbar):
-            plt.colorbar(c, shrink = shrink)
-    elif(len(args)==1):
-        if(isinstance(args[0], dolfin.cpp.mesh.Mesh)):
-            dolfin.common.plotting.plot(args[0])
-        else:
-            raise RuntimeError("Cannot plot an object in the FE space if no mesh is provided")
-    else:
-        raise RuntimeError("Too much inputs. Expected at most two non-keyword arguments (data values and mesh).")
+            uv = asvector(obj, space)
+            if(space.element().value_dimension(0)):
+                c = dolfin.common.plotting.plot(uv, vmin = vmin, vmax = vmax)
+            else:
+                c = dolfin.common.plotting.plot(uv)
+            if(colorbar):
+                plt.colorbar(c, shrink = shrink)
+    except:
+        raise RuntimeError("First argument should be either a dolfin.cpp.mesh.Mesh or a structure containing the dof values of some function (in which case 'space' must be != None).")
     plt.axis(axis)
     
-def gif(name, U, dt, T, mesh, obj = 'CG', deg = 1, axis = "off", figsize = (4,4)):
+def gif(name, U, dt, T, space, axis = "off", figsize = (4,4)):
     """Builds a GIF animation given the values of a functional object at multiple time steps.
     
     Input
@@ -294,5 +263,5 @@ def gif(name, U, dt, T, mesh, obj = 'CG', deg = 1, axis = "off", figsize = (4,4)
     step = N//frames
     def drawframe(i):
         plt.figure(figsize = figsize)
-        plot(u = U[i*step], mesh = mesh, obj = obj, deg = deg, axis = axis)
+        plot(u = U[i*step], space, axis = axis)
     gifs.save(drawframe, frames, name)
