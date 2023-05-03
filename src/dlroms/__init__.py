836e45dae41b884fc7205ca5a1a25a7b219437d5
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-05-03 10:36:33 +0200

    Update __init__.py

--- src/dlroms/__init__.py
+++ src/dlroms/__init__.py
@@ -1,6 +1,6 @@
 
 from dlroms.minns import L2, Local
 from dlroms.dnns import Dense, train
-from dlroms.roms import POD, project, snapshots, num2p, PODerrors
+from dlroms.roms import POD, project, snapshots, num2p, PODerrors, mre
 from dlroms.cores import CPU, GPU
 import dlroms.fespaces as fe
