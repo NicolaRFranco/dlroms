d1c8ee9a50417b15fdfa041f842a2f3b45eff6c6
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-05-03 09:40:20 +0200

    Update roms.py

--- src/dlroms/roms.py
+++ src/dlroms/roms.py
@@ -4,6 +4,15 @@ from scipy.linalg import solve as scisolve
 import torch
 from dlroms.cores import coreof, CPU, GPU
 
+def snapshots(n, sampler, core = GPU):
+    """Samples a collection of snapshots for a given FOM solver."""
+    mu, u = [], []
+    for seed in range(n):
+        mu0, u0 = sampler(seed)
+        mu.append(mu0)
+        u.append(u0)
+    mu, u = np.stack(mu), np.stack(u)
+    return core.tensor(mu, u)
 
 def POD(U, k):
     """Principal Orthogonal Decomposition of the snapshots matrix U into k modes."""
@@ -30,33 +39,42 @@ def projectdown(vbasis, u):
     """Given a sequence of basis vbasis = [V1,..., Vk], where Vj has shape (b, Nh), and
     a sequence of vectors u = [u1,...,uk], where uj has length Nh, yields the batched
     matrix vector multiplication [Vjuj], i.e. the sequence of basis coefficients."""
-    nh = np.prod(u[0].shape)
-    n, nb = vbasis.shape[:2]
-    return vbasis.reshape(n, nb, -1).matmul(u.reshape(-1,nh,1))
+    if(len(vbasis.shape)<3):
+      return projectdown(vbasis.unsqueeze(0), u)
+    else:
+      nh = np.prod(u[0].shape)
+      n, nb = vbasis.shape[:2]
+      return vbasis.reshape(n, nb, -1).matmul(u.reshape(-1,nh,1))
 
 def projectup(vbasis, c):
     """Given a sequence of basis vbasis = [V1,..., Vk], where Vj has shape (b, Nh), and
     a sequence of coefficients c = [c1,...,ck], where cj has length b, yields the batched
     matrix vector multiplication [Vj.Tcj], i.e. the sequence of expanded vectors."""
-    b = c.shape[1]
-    n, nb = vbasis.shape[:2]
-    return vbasis.reshape(n, nb, -1).transpose(dim0 = 1, dim1 = 2).matmul(c.reshape(-1,b,1)).reshape(-1, vbasis.shape[-1])
+    if(len(vbasis.shape)<3):
+      return projectup(vbasis.unsqueeze(0), c)
+    else:
+      b = c.shape[1]
+      n, nb = vbasis.shape[:2]
+      return vbasis.reshape(n, nb, -1).transpose(dim0 = 1, dim1 = 2).matmul(c.reshape(-1,b,1)).reshape(-1, vbasis.shape[-1])
 
 def project(vbasis, u, orth = True):
     """Given a sequence of basis vbasis = [V1,..., Vk], where Vj has shape (b, Nh), and
     a sequence of vectors u = [u1,...,uk], where uj has length Nh, yields the batched
     matrix vector multiplication [Vj'Vjuj], i.e. the sequence of reconstructed vectors."""
-    if(orth):
-        return project(gramschmidt(vbasis.transpose(1,2)).transpose(2,1), u, orth = False)
+    if(len(vbasis.shape)<3):
+        return project(vbasis.unsqueeze(0), u, orth)
     else:
-        return projectup(vbasis, projectdown(vbasis, u))
+        if(orth):
+            return project(gramschmidt(vbasis.transpose(1,2)).transpose(2,1), u, orth = False)
+        else:
+            return projectup(vbasis, projectdown(vbasis, u))
 
 def gramschmidt(V):
     """Orthonormalizes a collection of matrices. V should be a torch tensor in the format batch dimension x space dimension x number of basis."""
     return torch.linalg.qr(V, mode = 'reduced')[0]
 
 def PAs(V1, V2, orth = True):
-    """List of principal angles between the subspaces in V1 and V2. The Vj's should be in the format 
+    """List of principal angles between the subspaces in V1 and V2. The Vj's should be in the format
     batch dimension x space dimension x number of basis."""
     if(orth):
         A1, A2 = gramschmidt(V1), gramschmidt(V2)
