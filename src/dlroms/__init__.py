b168ccf621874db9d6bd465cc4204f2483202166
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-09-26 15:18:47 +0200

    Update __init__.py

--- src/dlroms/__init__.py
+++ src/dlroms/__init__.py
@@ -6,5 +6,6 @@ import dlroms.fespaces as fe
 import matplotlib.pyplot as plt
 
 def plot(*args, **kwargs):
+  from torch import Tensor
   newargs = [(a if not isinstance(a, Tensor) else a.cpu().numpy())  for a in args]
   plt.plot(*newargs, **kwargs)
