27177d2dc59e850207262c791a86d63de5d9e8be
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-09-26 15:11:04 +0200

    Update __init__.py

--- src/dlroms/__init__.py
+++ src/dlroms/__init__.py
@@ -3,3 +3,8 @@ from dlroms.dnns import Dense, train, Clock, num2p
 from dlroms.roms import POD, project, snapshots, PODerrors, mre, mse, ROM, euclidean, boxplot, regcoeff, PODNN, DLROM, mrei, msei
 from dlroms.cores import CPU, GPU
 import dlroms.fespaces as fe
+import matplotlib.pyplot as plt
+
+def plot(*args, **kwargs):
+  newargs = [(a if not isinstace(a, Tensor) else a.cpu().numpy())  for a in args]
+  plt.plot(*newargs, **kwargs)
