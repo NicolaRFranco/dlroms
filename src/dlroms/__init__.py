c15acab201d154110576c32a130344c31935d716
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-10-05 19:41:35 +0200

    Update __init__.py

--- src/dlroms/__init__.py
+++ src/dlroms/__init__.py
@@ -17,7 +17,27 @@ from dlroms.cores import CPU, GPU
 import dlroms.fespaces as fe
 import matplotlib.pyplot as plt
 
+from IPython.display import clear_output
+import os
+
 def plot(*args, **kwargs):
   from torch import Tensor
   newargs = [(a if not isinstance(a, Tensor) else a.cpu().numpy())  for a in args]
   plt.plot(*newargs, **kwargs)
+
+def setcolab():
+  try:
+    import dolfin
+  except ImportError:
+    print("Installing fenics... this should take about 30-60 seconds.")
+    os.system('wget "https://fem-on-colab.github.io/releases/fenics-install.sh" -O "/tmp/fenics-install.sh" && bash "/tmp/fenics-install.sh"')
+
+  try:
+    import gmsh
+  except ImportError:
+    print("Installing gmsh... this should take about 30-60 seconds.")
+    os.system('wget "https://fem-on-colab.github.io/releases/gmsh-install.sh" -O "/tmp/gmsh-install.sh" && bash "/tmp/gmsh-install.sh"')
+
+  clear_output(wait = True)
+  print("Both fenics and gmsh are installed.")
+  
