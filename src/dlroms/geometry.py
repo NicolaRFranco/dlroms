468b64709435948eb1fa8fdeae56eb9a9faab00b
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-10-05 20:10:07 +0200

    Update geometry.py

--- src/dlroms/geometry.py
+++ src/dlroms/geometry.py
@@ -1,3 +1,17 @@
+# Written by: Nicola Rares Franco, Ph.D. (MOX, Department of Mathematics, Politecnico di Milano)
+# 
+# Scientific articles based on this Python package:
+# [1] Franco et al. (2023). A deep learning approach to reduced order modelling of parameter dependent partial differential equations
+#     DOI: https://doi.org/10.1090/mcom/3781
+# [2] Franco et al. (2023). Approximation bounds for convolutional neural networks in operator learning, Neural Networks.
+#     DOI: https://doi.org/10.1016/j.neunet.2023.01.029
+# [3] Franco et al. (2023). Mesh-Informed Neural Networks for Operator Learning in Finite Element Spaces, Journal of Scientific Computing.
+#     DOI: https://doi.org/10.1007/s10915-023-02331-1
+#
+# Please cite the Author if you use this code for your work/research.
+#
+# Note: this is an auxiliary library, only to be used if mshr is not available.
+
 import dolfin
 import numpy
 import gmsh
