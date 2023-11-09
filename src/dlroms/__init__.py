a576ba01ffd4b3f507098ee7616b1140ab839e1d
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2023-11-09 10:27:27 +0100

    Update __init__.py

--- src/dlroms/__init__.py
+++ src/dlroms/__init__.py
@@ -1,12 +1,22 @@
 # Written by: Nicola Rares Franco, Ph.D. (MOX, Department of Mathematics, Politecnico di Milano)
 # 
 # Scientific articles based on this Python package:
-# [1] Franco et al. (2023). A deep learning approach to reduced order modelling of parameter dependent partial differential equations
-#     DOI: https://doi.org/10.1090/mcom/3781
-# [2] Franco et al. (2023). Approximation bounds for convolutional neural networks in operator learning, Neural Networks.
+#
+# [1] Franco et al., Mathematics of Computation (2023).
+#     A deep learning approach to reduced order modelling of parameter dependent partial differential equations.
+#     DOI: https://doi.org/10.1090/mcom/3781.
+#
+# [2] Franco et al., Neural Networks (2023).
+#     Approximation bounds for convolutional neural networks in operator learning.
 #     DOI: https://doi.org/10.1016/j.neunet.2023.01.029
-# [3] Franco et al. (2023). Mesh-Informed Neural Networks for Operator Learning in Finite Element Spaces, Journal of Scientific Computing.
+#
+# [3] Franco et al., Journal of Scientific Computing (2023). 
+#     Mesh-Informed Neural Networks for Operator Learning in Finite Element Spaces.
 #     DOI: https://doi.org/10.1007/s10915-023-02331-1
+#
+# [4] Vitullo, Colombo, Franco et al., Finite Elements in Analysis and Design (2024).
+#     Nonlinear model order reduction for problems with microstructure using mesh informed neural networks.
+#     DOI: https://doi.org/10.1016/j.finel.2023.104068
 #
 # Please cite the Author if you use this code for your work/research.
  
