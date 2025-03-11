95a7b6b2aab91bfe0698b8190947dd40862f4107
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2025-03-11 17:20:51 +0100

    Add files via upload

---
+++ src/dlroms/testcases/problems.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def UniformSampler(p, seed):
+  np.random.seed(seed)
+  return np.random.rand(p)
+
+class Problem(object):
+    def __init__(self, FOM, parameters, sampler = UniformSampler):
+        self.FOM = FOM
+        self.parameters = parameters
+        self.sampler = sampler
+
+    def generate(self, N, filename, verbose = True):
+        p = len(self.parameters['Parameter'])
+        pmax = np.array(self.parameters['Max'])
+        pmin = np.array(self.parameters['Min'])
+        
+        def FOMsampler(seed):
+            mu = self.sampler(p, seed = seed)*(pmax-pmin) + pmin
+            u = self.FOM(mu)
+            return mu, u
+
+        from dlroms.roms import snapshots
+        snapshots(N, sampler = FOMsampler, filename = filename, verbose = verbose)
+
+    def params_summary(self):
+        import pandas as pd
+        return pd.DataFrame(self.parameters)
+
+    def FOM_summary(self):
+        from dlroms.dnns import Clock
+        p = len(self.parameters['Parameter'])
+        pmax = np.array(self.parameters['Max'])
+        pmin = np.array(self.parameters['Min'])
+        clock = Clock()
+        clock.start()
+        u0 = self.FOM(self.sampler(p, seed = 0)*(pmax-pmin) + pmin)
+        clock.stop()
+        print("PDE parameters:\t%d." % p)
+        print("FOM dimension:\t%d." % len(u0))
+        print("FOM exec. time:\t%s." % clock.elapsedTime())
+
+def FOMdata(label):
+    import importlib
+    module_name = f"dlroms.testcases.{label}"
+    try:
+        module = importlib.import_module(module_name)
+        return getattr(module, "FOMsolver"), getattr(module, "parameters")
+    except (ModuleNotFoundError, AttributeError) as e:
+        raise ImportError(f"Could not import 'FOMsolver' and 'parameters' from {module_name}") from e
+
+def generate_data(problem_label, ndata, summary = True, filename = None):
+    problem = Problem(*FOMdata(problem_label))
+    if(summary):
+        problem.FOM_summary()
+        print("\n")
+        print(problem.params_summary())
+        print("\n")
+        name = filename if(not (filename is None)) else (problem_label + "_snapshots")
+        problem.generate(ndata, filename = name + ".npz")    
---
+++ src/dlroms/testcases/rod.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+from IPython.display import clear_output as clc
+from dlroms import*
+import numpy as np
+import gdown
+
+gdown.download(id = "1-YmekOC5d1ACFsCAO0IEg5AuWji03SxF", output = "rod.xml", quiet = True)
+mesh = fe.loadmesh("rod.xml")
+Vh = fe.space(mesh, 'CG', 1)
+clc()
+
+parameters = {'Parameter':['T1', 'T2', 'T3', 'log10(f)', 'log(10)(alpha)'],
+              'Min': [80, 80, 80, 1.5, -3.5],
+              'Max': [120, 120, 120, 5, -1.5],
+              'Meaning': ['Contact temperature']*3 + ['External heat source', 'Thermal radiation coefficient']}
+
+def FOMsolver(mu):
+    from fenics import Function, TestFunction, Constant, DirichletBC, dx, inner, grad, exp, solve
+    
+    # Portions of domain boundary subject to Dirichlet b.c.
+    def leftCircle(x, on):
+        return on and ((x[0]+2)**2 + x[1]**2)**0.5 < 0.5
+    def rightCircle(x, on):
+        return on and ((x[0]-2)**2 + x[1]**2)**0.5 < 0.8
+    def rag(x, on):
+        return on and (x[0]>-0.3) and (x[0]<0.3) and (x[1]>0)
+    
+    dbc1 = DirichletBC(Vh, Constant(mu[0]), leftCircle)
+    dbc2 = DirichletBC(Vh, Constant(mu[1]), rightCircle)
+    dbc3 = DirichletBC(Vh, Constant(mu[2]), rag)
+    
+    u, v = Function(Vh), TestFunction(Vh)
+    L = inner((1000+exp(u/8.0))*grad(u), grad(v))*dx - Constant(10**mu[3])*v*dx + (10**mu[4])*(u**4)*v*dx
+    clc()
+    
+    u.vector()[:] = (mu[0]+mu[1]+mu[2])/3.0 # Initial guess for nonlinear solver
+    solve(L == 0, u, [dbc1, dbc2, dbc3])
+    return u.vector()[:]
