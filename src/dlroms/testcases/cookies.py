9e4e758ee018149855be422e798ac64007a1a7c4
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2025-03-14 16:50:55 +0100

    Update cookies.py

--- src/dlroms/testcases/cookies.py
+++ src/dlroms/testcases/cookies.py
@@ -4,6 +4,11 @@ from fenics import inner, grad, dx
 from IPython.display import clear_output as clc
 from scipy.sparse.linalg import spsolve
 
+parameters = {'Parameter':['log10(s0)', 'log10(s1)', 'log10(s2)', 'log10(s3)', 'log10(s4)'],
+              'Min': [-2, -2, -2, -2],
+              'Max': [ 2,  2,  2,  2],
+              'Meaning': ['Conductivity jth cookie']*4}
+
 # Problem data and discretization
 domain = fe.rectangle((0, 0), (1, 1))
 mesh = fe.mesh(domain, stepsize = 0.01, structured = True)
@@ -48,8 +53,8 @@ fe.applyBCs(fh, Vh, bc)
 clc()
 
 def FOMsolver(mu):
-  
-  Ah = A_out + mu[0]*A0 + mu[1]*A1 + mu[2]*A2 + mu[3]*A3
+  xi = 10.0**mu
+  Ah = A_out + xi[0]*A0 + xi[1]*A1 + xi[2]*A2 + xi[3]*A3
   Ah = fe.applyBCs(Ah, Vh, bc)
 
   uh = spsolve(Ah, fh)
