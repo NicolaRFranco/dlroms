9b0456d8150c85f264e714107a4a7723e06c5f68
NicolaRFranco <98050979+NicolaRFranco@users.noreply.github.com>
2025-03-14 16:42:14 +0100

    Add files via upload

---
+++ src/dlroms/testcases/cookies.py
@@ -0,0 +1,57 @@
+from dlroms import*
+import numpy as np
+from fenics import inner, grad, dx
+from IPython.display import clear_output as clc
+from scipy.sparse.linalg import spsolve
+
+# Problem data and discretization
+domain = fe.rectangle((0, 0), (1, 1))
+mesh = fe.mesh(domain, stepsize = 0.01, structured = True)
+
+Vh = fe.space(mesh, 'CG', 1)
+Dh = fe.space(mesh, 'DG', 0)
+
+def diskIndicator(xcenter, ycenter, radius):
+  return lambda x: ((x[0]-xcenter)**2 + (x[1]-ycenter)**2)**0.5 < radius
+
+centers = np.array([[0.25, 0.25],
+                    [0.25, 0.75],
+                    [0.75, 0.25],
+                    [0.75, 0.75]])
+
+# Pre-assembling the matrices A_out, A0, A1, A2, A3
+a_out = lambda u, v: inner(0.01*grad(u), grad(v))*dx
+A_out = fe.assemble(a_out, Vh)
+
+Ajs = []
+for j in range(4):
+  sigmaj = diskIndicator(centers[j, 0], centers[j, 1], 0.15)
+  sigmaj_h = fe.interpolate(sigmaj, Dh)
+
+  aj = lambda u, v: inner(sigmaj_h*grad(u), grad(v))*dx
+  Aj = fe.assemble(aj, Vh)
+
+  Ajs.append(Aj)
+
+A0, A1, A2, A3 = Ajs
+
+# Pre-assembling the RHS
+f = fe.interpolate(1.0, Vh)
+F = lambda v: f*v*dx
+fh = fe.assemble(F, Vh)
+
+# Boundary conditions
+boundary = lambda x: True
+bc = fe.DirichletBC(boundary, 0.0)
+fe.applyBCs(fh, Vh, bc)
+
+clc()
+
+def FOMsolver(mu):
+  
+  Ah = A_out + mu[0]*A0 + mu[1]*A1 + mu[2]*A2 + mu[3]*A3
+  Ah = fe.applyBCs(Ah, Vh, bc)
+
+  uh = spsolve(Ah, fh)
+
+  return uh
